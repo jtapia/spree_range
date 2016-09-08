c956387a3108b563fe1defe54c07c68cbdb5cc23
Jonathan Tapia <jonathan.tapia@magmalabs.io>
2016-09-07 21:14:58 -0500

    Add quick improvements

--- db/migrate/20160509174150_add_spree_range_categories_table.rb
+++ db/migrate/20160509174150_add_spree_range_categories_table.rb
@@ -1,15 +1,18 @@
 class AddSpreeRangeCategoriesTable < ActiveRecord::Migration
   def change
     create_table :spree_range_categories do |t|
-      t.integer  :range_id
-      t.string   :name
-      t.string   :description
-      t.string   :image_file_name
-      t.string   :image_content_type
-      t.integer  :image_file_size
-      t.datetime :image_updated_at
+      t.integer     :range_id
+      t.string      :name
+      t.string      :description
+      t.attachment  :image
+      t.string      :image_file_name
+      t.string      :image_content_type
+      t.integer     :image_file_size
+      t.datetime    :image_updated_at
 
       t.timestamps
+    end
+  end
     end
   end
 end
